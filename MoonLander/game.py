c577b230076c7ac38e35a82a8e041ee825c0cca8
KemptCode <bar19026@byui.edu>
2022-07-15 23:33:27 -0600

    Arrow Keys

--- MoonLander/game.py
+++ MoonLander/game.py
@@ -1,44 +1,65 @@
 import pygame
-pygame.init()
-
 import time
-import _thread
 
 from terrain import Terrain
 from lander import Lander
+from Point2D import Point2D
 
 CANVAS_WIDTH = 600
 CANVAS_HEIGHT = 400
 
-# Game window setup
-screen = pygame.display.set_mode([CANVAS_WIDTH, CANVAS_HEIGHT])
+class Game:
+    def __init__(self) -> None:
+    # Game elements setup
+        # BackGround
 
-# Game elements setup
-    # BackGround
-    # Terrain
-    # Lander
-
-
-#Game loop
-running = True
-while running:
-
-    # Handle Events
-    for event in pygame.event.get():
-
-        # EXIT
-        if event.type == pygame.QUIT:
-            running = False
-    
-    # Calc time delta since last frame
-
-    # Render Frame
-        # Background
         # Terrain
-        # Lander
+        self.terrain = Terrain()
 
-    # Update Physics
         # Lander
+        self.lander = Lander(Point2D) # add starting position
 
-    # Timing
-    time.sleep(.04)
+    def run(self):
+        pygame.init()
+        # Game window setup
+        screen = pygame.display.set_mode([CANVAS_WIDTH, CANVAS_HEIGHT])
+
+        #Game loop
+        running = True
+        while running:
+
+            # Handle Events
+            for event in pygame.event.get():
+
+                # EXIT
+                if event.type == pygame.QUIT:
+                    running = False
+
+                if event.type == pygame.K_UP:
+                    pass
+
+                if event.type == pygame.K_DOWN:
+                    pass
+
+                if event.type == pygame.K_LEFT:
+                    pass
+
+                if event.type == pygame.K_RIGHT:
+                    pass
+            
+            # Calc time delta since last frame
+
+            # Render Frame
+                # Background
+                # Terrain
+                # Lander
+
+            # Update Physics
+                # Lander
+
+            # Timing
+            time.sleep(.04)
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
