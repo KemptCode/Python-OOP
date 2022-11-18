66ed2482fc79d9656368cdba120bc42c85c1d5f1
KemptCode <bar19026@byui.edu>
2022-11-17 22:47:17 -0700

    Terrain height calculations

--- MoonLander/terrain.py
+++ MoonLander/terrain.py
@@ -9,6 +9,8 @@ class Terrain():
     '''
     def __init__(self, width, height, num_points : int = 20) -> None:
         self.__generate_terrain( width, height, num_points)
+        self.width = width
+        self.num_points = num_points
 
     def __generate_terrain(self, width, height, num_points : int) -> None:
         self.points = []
@@ -19,6 +21,17 @@ class Terrain():
     def get_platform_position(self) -> Point2D:
         return self.platform_pos
 
-    def get_terrain_height_at(x : int) -> int:
-        pass
+    def get_terrain_height_at(self, x : int) -> int:
+        if x < 0 or self.width < x:
+            return 0
+
+        i = x // (self.width / self.num_points)
+        p1, p2 = self.points[i], self.points[i+1]
+
+        dx = p2.x - p1.x
+        dy = p2.y - p1.y
+        slope = dy/dx
+        y = slope * (x + p1.x) - p1.y
+
+        return y
 
