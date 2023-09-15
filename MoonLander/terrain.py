cf90f7fdb4fd9c5762eb68d76e3b51f55c7a091b
HalfMatt404 <135181812+HalfMatt404@users.noreply.github.com>
2023-09-14 19:50:19 -0600

    platform height logic

--- MoonLander/terrain.py
+++ MoonLander/terrain.py
@@ -10,22 +10,35 @@ class Terrain():
     Sets a platform for landing
     '''
 
-    def __init__(self, width: int, height: int, num_points: int = 20) -> None:
+    def __init__(self, width: int, height: int, num_segments: int = 20) -> None:
         self.width = width
         self.height = height
-        self.num_segments = num_points - 1
-        self.__generate_terrain(width, height, num_points)
+        self.num_segments = num_segments - 1
+        self.__generate_terrain(width, height, num_segments)
 
     def __generate_terrain(self, width: int, height: int, num_segments: int) -> None:
         self.points = []
         for i in range(num_segments + 1):
             self.points.append(Point2D(i * width//num_segments,
                                random.randint(height//2, height)))
-        platform_x = random.randint(0, width)
-        self.platform_pos = Point2D(platform_x, self.get_terrain_height_at(platform_x) - 20)
+        platform_x = random.randint(0, num_segments * (width//num_segments) - 66)
+        self.platform_pos = Point2D(platform_x, self.highest_point_in_range(platform_x, platform_x + 64) - 20)
 
     def get_platform_position(self) -> Point2D:
         return self.platform_pos
+    
+    def highest_point_in_range(self, x1, x2):
+        array = [self.get_terrain_height_at(x1), self.get_terrain_height_at(x2)]
+
+        terrain_width = (self.width // self.num_segments)
+        current_index = int(x1 // terrain_width)
+
+        while 0 <= current_index < self.num_segments and current_index * terrain_width <= x2:
+            if x1 <= current_index * terrain_width:
+                array.append(self.points[current_index].y)
+            current_index += 1
+
+        return min(array)
 
     def get_terrain_height_at(self, x: int) -> int:
         if x < 0 or self.width < x:
